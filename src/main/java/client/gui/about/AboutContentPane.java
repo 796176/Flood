f93c814cebc3212f5f06eb59eb1db3e4b6faf369
yav <796176@protonmail.com>
2024-09-08 01:31:48 +0300

    Bump the version up to 0.4.1

--- src/main/java/client/gui/about/AboutContentPane.java
+++ src/main/java/client/gui/about/AboutContentPane.java
@@ -30,7 +30,7 @@ class AboutContentPane extends JScrollPane{
 		JTextArea textArea = new JTextArea(
 			"""
 				Flood
-				Version 0.4.0
+				Version 0.4.1
 				
 				Flood GUI client is a frontend for Flood network inspection tool.
 				
